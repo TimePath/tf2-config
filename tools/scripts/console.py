9e17ce737c95f1a65e7678db0ff284855ba1c504
TimePath <andrew.hardaker1995@gmail.com>
2013-11-29 18:47:33 +1100

    Implemented time based read buffering

--- tools/scripts/console.py
+++ tools/scripts/console.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import socket
+from select import select
+import time
 
 class Console:
 	def __init__(self, host="localhost", port=12345):
@@ -7,12 +9,24 @@ class Console:
 		self.s = socket.socket()
 		self.s.connect((host, port))
 		self.f = self.s.makefile("rw")
-	
-	def read(self):
-		line = self.f.readline()[:-1]
-		if line == self.lastcmd: return self.read()
-		return line
-	
+		
+	def read(self, timeslice=0):
+		if timeslice:
+			lines = []
+			end = time.time() + timeslice
+			while True:
+				now = time.time()
+				if now >= end: break
+				r, w, x = select([self.s], [], [], end - now)
+				if len(r) == 0: break
+				line = self.read()
+				lines.append(line)
+			return lines
+		else:
+			line = self.f.readline()[:-1] # Remove trailing \n
+			if line == self.lastcmd: return self.read()
+			return line
+		
 	def send(self, cmd):
 		self.lastcmd = cmd
 		msg = "%s\n" % cmd
