3d92dcbaf8ea7b509b41d4202f2851a7e1781f4c
TimePath <andrew.hardaker1995@gmail.com>
2013-11-29 18:48:48 +1100

    Less obtrusive crosshair

--- tools/overlay/xhair.py
+++ tools/overlay/xhair.py
@@ -4,26 +4,29 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import time
 
+rings = False
+
 class Xhair(overlay.OSD):
 	
 	def render(self, qp):
 		t = (time.time() - self.start) % (10 * 360)
-		speed = 58
-		m = 3
-		qp.setPen(QPen(QColor.fromRgbF(.9, .9, .9, .75), 3))
-		for i in range(m):
-			self.arc(qp, self.size(), 40, 1, -1.5*t*speed+(360/m)*i, (360 / (m+1)))
-			
-			self.arc(qp, self.size(), 40, 5, t*speed+(360/m)*i, .25*(360 / (m+1)))
-			self.arc(qp, self.size(), 40, 5, 180+t*speed+(360/m)*i, .25*(360 / (m+1)))
+		if rings:
+			speed = 58
+			m = 3
+			qp.setPen(QPen(QColor.fromRgbF(.9, .9, .9, .75), 3))
+			for i in range(m):
+				self.arc(qp, self.size(), 40, 1, -1.5*t*speed+(360/m)*i, (360 / (m+1)))
+				
+				self.arc(qp, self.size(), 40, 5, t*speed+(360/m)*i, .25*(360 / (m+1)))
+				self.arc(qp, self.size(), 40, 5, 180+t*speed+(360/m)*i, .25*(360 / (m+1)))
 		
 		qp.setPen(Qt.transparent)
-		qp.setBrush(QColor.fromRgbF(0.8, 0.8, 0.8, 0.5))
-		self.arc(qp, self.size(), 16, 0, 0, 360, qp.drawPie)
+		qp.setBrush(QColor.fromRgbF(0.8, 0.8, 0.8, 0.1))
+		self.arc(qp, self.size(), 32, 0, 0, 360, qp.drawPie)
 		
 		qp.setPen(Qt.transparent)
-		qp.setBrush(QColor.fromRgbF(1, 1, 1, 1))
-		self.arc(qp, self.size(), 2, 0, 0, 360, qp.drawPie)
+		qp.setBrush(QColor.fromRgbF(1, 1, 1, .5))
+		self.arc(qp, self.size(), 4, 0, 0, 360, qp.drawPie)
 
 if __name__ == "__main__":
 	Xhair().start()
